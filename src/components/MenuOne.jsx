985287f19c7eab0684ebf8fc6b96f3923ff257ee
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2025-11-05 01:14:14 -0800

    test3

--- src/components/MenuOne.jsx
+++ src/components/MenuOne.jsx
@@ -82,7 +82,7 @@ useGSAP(() => {
   
   
   return (
-    <div className=" h-auto justify-center flex items-center bg-[#533116]">
+    <div className=" h-auto pt-30 -mt-10 sm:mt-0  sm:pt-10 justify-center flex items-center bg-[#533116]">
       <div className="bg-[#9b663a] mb-5 h-auto flex  rounded-4xl w-[90%]">
         <div className="lg:w-1/2 w-full h-auto rounded-4xl  text-[#f2e5bc] ">
 
